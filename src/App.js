a5b6a4ade8f368670f364bf4744bb6c58af78846
ork821 <ork821@mail.ru>
2019-06-14 16:27:47 +0300

    Added Bootstrap React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import Clock from "./Clock";
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'December 25, 2019',
+            deadline: 'December 31, 2019',
             newDeadLine: '',
         }
     }
@@ -21,13 +22,13 @@ class App extends Component{
         return(
             <div className="App">
                 <h2>Countdown to {this.state.deadline}</h2>
-                <Clock />
-                <div>
-                    <input type="text"
+                <Clock deadline={this.state.deadline}/>
+                <Form inline>
+                    <FormControl type="text"
                            placeholder="new Date"
                            onChange={(ev) => this.setState({newDeadLine: ev.target.value})}/>
-                    <button onClick={this.changeDeadline}>Submit</button>
-                </div>
+                    <Button onClick={this.changeDeadline}>Submit</Button>
+                </Form>
 
             </div>
         )
--- src/Clock.js
+++ src/Clock.js
@@ -10,16 +10,38 @@ class Clock extends Component{
             seconds: 0
         }
     }
-
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline);
+    }
+    componentDidMount() {
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
     render() {
         return(
             <div>
-                <div className="clockDays">{this.state.days} days</div>
-                <div className="clockHours">{this.state.hours} hours</div>
-                <div className="clockMinutes">{this.state.minutes} minutes</div>
-                <div className="clockSeconds">{this.state.seconds} seconds</div>
+                <div className="clockDays">{this.leadingZero(this.state.days)} days</div>
+                <div className="clockHours">{this.leadingZero(this.state.hours)} hours</div>
+                <div className="clockMinutes">{this.leadingZero(this.state.minutes)} minutes</div>
+                <div className="clockSeconds">{this.leadingZero(this.state.seconds)} seconds</div>
             </div>
         )
+    }
+
+    leadingZero = (num) => {
+        return (num < 10) ? '0' + num : num;
+    };
+
+
+    getTimeUntil = (deadline) => {
+        const date = Date.parse(deadline) - Date.parse(new Date());
+        const seconds = Math.floor((date/1000)%60);
+        const minutes = Math.floor(((date/1000)/60)%60);
+        const hours = Math.floor((date/(1000*60*60))%24);
+        const days = Math.floor(date/(1000*60*60*24));
+        this.setState({
+            days, hours, minutes, seconds,
+        })
+
     }
 }
 
